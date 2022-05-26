637249997df08a2249b6ba99bc0f2845619e227c
Astrovik3 <juantomaz13@gmail.com>
2022-05-25 22:16:04 -0300

    add circularProgress

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Grid } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 import Item from "./Item";
 
 const ItemList = ({items}) => {
@@ -9,7 +11,9 @@ const ItemList = ({items}) => {
       {
         items.length > 0
         ? items.map(item => <Item ids={item.ids} name={item.name} price={item.price} photo={item.photo}/> )
-        : <p>cargando.....</p>
+        : <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+            <CircularProgress />
+          </Box>
       }
     </Grid>
   );
